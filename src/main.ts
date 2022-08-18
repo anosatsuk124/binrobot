1e227515a297d18d197f5a58fc2c7635803a7f9e
Satsuki Akiba <anosatsuk124@Satsukis-MacBook-Pro.local>
2022-08-19 01:46:44 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import './style.css';
 import { app } from './app';
 
+import { open } from '@tauri-apps/api/dialog';
+import { emit } from '@tauri-apps/api/event';
+
+document
+    .querySelector<HTMLDivElement>('#settings')
+    ?.querySelector<HTMLButtonElement>('#open-dialog')
+    ?.addEventListener('click', async () => {
+        const execCommandPath = await open({
+            multiple: false
+        });
+
+        if (execCommandPath != null) {
+            emit('execCommandPath', execCommandPath);
+        }
+    });
+
 document.querySelector<HTMLDivElement>('#app')!.appendChild(app.view);
